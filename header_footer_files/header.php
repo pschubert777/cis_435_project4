8f114bfa373ca826b6905e61ee2a362e85e09048
pschuber <pschuber@umich.edu>
2020-11-30 12:33:38 -0500

    Revert "Update header.php"
    This reverts commit 43d21fd9711662afff513ea8c001736599cc0272.

--- header_footer_files/header.php
+++ header_footer_files/header.php
@@ -20,21 +20,12 @@ session_start();
 <body>
     <header>
         <nav>
-                <li><a href="/cis_435_project4/3_menu_page-admin-customer"> Menu</a></li>
-            <?php 
-            if ($_SESSION['userType']=='customer'):
-            ?>
-                <li><a href="/cis_435_project4/deposit_page.php">Deposit Transfer Withdraw</a></li> 
-                <li><a href="/cis_435_project4/6_create-checking-customer/create_checking-cd.php">Add New Account</a></li>
-            <?php endif?>
+            <li><a href="/cis_435_project4/menu.php"> Menu</a></li>
 
-
-            <?php 
-            if ($_SESSION['userType']=='admin'):
-            ?>
-                <li><a href="/cis_435_project4/8_bank_metrics-admin/metrics.php">Records</a></li>
-            <?php endif?>
-                <li><a href="/cis_435_project4/9_10search_and_modify-admin-customer">Modify Account</a></li>
-                <li><a href="/cis_435_project4/11_logout/logout.php"> Logout</a></li>
+            <li><a href="/cis_435_project4/deposit_page.php">Deposit Transfer Withdraw</a></li>
+            <li><a href="/cis_435_project4/creation_account_page.php">Add New Account</a></li>
+            <li><a href="/cis_435_project4/historical_data_page.php">Records</a></li>
+            <li><a href="/cis_435_project4/9_10search_and_modify-admin-customer">Modify Account</a></li>
+            <li><a href="/cis_435_project4/11_logout/logout.php"> Logout</a></li>
         </nav>
     </header>
